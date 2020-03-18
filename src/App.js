8eddb53ca0ad4c3ec2a4ecd7a291273a76572f1a
Ishaan Sehgal <ishaanforthewin@gmail.com>
2020-03-18 18:15:56 -0500

    Added nodemon and learned basic app linking

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import { Link } from "react-router-dom"
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>React Electron Boilerplate</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Link className="App-link" to="/about">Link to the About Page</Link>
-      </header>
-      
-    </div>
-  );
+// import logo from './logo.svg';
+
+export default class App extends React.Component{
+    render(){
+        return (
+            <React.Fragment>
+               <h1> Hello World </h1>
+               <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+               > Learn React </a>
+                <br />
+                <Link className="App-link" to="/about">Link to the About Page</Link>
+            </React.Fragment>
+        );
+    }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>React Electron Boilerplate</p>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <Link className="App-link" to="/about">Link to the About Page</Link>
+//       </header>
+//
+//     </div>
+//   );
+// }
+//
+// export default App;
